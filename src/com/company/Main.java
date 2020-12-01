9db9bf1c46f509ccbe84ee6dc1f2885f4526a4f6
Esenkul - uulu-Ilim <ilim.orozkulov04@gmail.com>
2020-12-01 22:32:23 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String LearnLearnAndOnlyLearn = "В теории, теория и практика неразделимы. На практике это не так.";
+
+
+        int NUM = 16;
+        String Word = " Летний мальчик ";
+        System.out.println(NUM + Word);
+
+        if (NUM > 0) {
+
+            System.out.println("Вы сохранили положительное число");
+        } else {
+        }
+
+
+    }
+}
